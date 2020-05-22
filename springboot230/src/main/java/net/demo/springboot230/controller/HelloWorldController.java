7abcf0bcb7237b0806097c41dbbd69226bc5a531
Marco Antonio Muñiz Ochoa <marco.muniz.ochoa@gmail.com>
2020-05-22 00:19:25 -0700

    Add SpringBoot project version 2.3.0

---
+++ springboot230/src/main/java/net/demo/springboot230/controller/HelloWorldController.java
@@ -0,0 +1,17 @@
+package net.demo.springboot230.controller;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+  private final AtomicLong counter = new AtomicLong();
+
+  @RequestMapping("/helloWorld")
+  public String helloWorld() {
+    return String.format("Request # %s", counter.incrementAndGet());
+  }
+
+}
