e54fa1466b91c3708588a8e063c43b2654938c8a
Marco Antonio Muñiz Ochoa <marco.muniz.ochoa@gmail.com>
2020-05-21 22:11:43 -0700

    Add load_test bash script

---
+++ load_test.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+# Tools used in this script:
+# - http: https://httpie.org/
+# - hey: https://github.com/rakyll/hey
+# - wrk: https://github.com/wg/wrk
+
+start=`date +%s`
+
+ENDPOINT=http://127.0.0.1:8080/api/helloWorld
+
+echo "ENDPOINT: $ENDPOINT"
+
+echo '==> Simple request'
+
+http :8080/api/helloWorld
+
+echo '==> First load'
+
+hey -n 100000 -c 100 $ENDPOINT
+
+sleep 10
+
+echo '==> Second request'
+
+hey -n 100000 -c 100 $ENDPOINT
+
+sleep 10
+
+echo '==> Third request'
+
+hey -n 100000 -c 100 $ENDPOINT
+
+sleep 10
+
+echo '==> hit it for 30s'
+
+wrk -c 100 -d 30s -t 10 $ENDPOINT
+
+echo '==> Last simple request'
+
+http :8080/api/helloWorld
+
+end=`date +%s`
+
+echo "Total time in seconds: $((end-start))"
+
